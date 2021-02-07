0ef809a7c5d810a629c659f31363dd73977bbda5
maxim5 <maxim.podkolzine@gmail.com>
2021-02-07 15:54:23 +0300

    save site structure

---
+++ header.js
@@ -0,0 +1,41 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+      <div class="navbar-header">\
+        <a class="navbar-brand brand" href="index.html">CS224n Home</a>\
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+          </button>\
+      </div>\
+\
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+\
+          <ul class="nav navbar-nav navbar-right">\
+              <li><a href="index.html#coursework">Coursework</a></li>\
+              <li><a href="index.html#schedule">Schedule</a></li>\
+              <li><a href="office_hours.html">Office Hours</a></li>\
+              <li><a href="https://www.youtube.com/playlist?list=PLoROMvodv4rOhcuXMZkNm7j3fVwBBY42z">Lecture Videos</a></li>\
+              <li><a href="project.html">Project Reports</a></li>\
+              <li><a href="https://piazza.com/stanford/winter2019/cs224n">Piazza</a></li>\
+          </ul>\
+      </div>\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <a href="http://nlp.stanford.edu/">\
+    <img src="images/stanford-nlp-logo-160.jpg" class="logo-left">\
+  </a>\
+  <a href="http://stanford.edu/">\
+    <img src="images/stanfordlogo.jpg" class="logo-right">\
+  </a>\
+  <h1>CS224n: Natural Language Processing with Deep Learning</h1>\
+  <h3>Stanford / Winter 2019</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
